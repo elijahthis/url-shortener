5b1f5c014cc8924a354b10ad91397aad8403e0bb
elijahthis <emperorelijah007@gmail.com>
2023-02-08 02:19:11 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	fmt.Println("Mainnnn")
+}
